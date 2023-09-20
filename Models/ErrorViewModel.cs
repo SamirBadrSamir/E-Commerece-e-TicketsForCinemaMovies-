5754a322424d4d064c2135fb7bb68f4c17101f8e
SamirBadrSamir <samirbadr2000p@gmail.com>
2023-09-20 10:02:47 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace E_Commerece_e_TicketsForCinemaMovies_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
